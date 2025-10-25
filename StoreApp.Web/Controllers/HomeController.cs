20eae19b3b0c521210f9deb6f613068c0c5192e7
Bugracanu <bugracanusta@gmail.com>
2025-10-25 14:42:06 +0300

    Libraries

--- StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using StoreApp.Data.Abstract;
 using StoreApp.Data.Concrete;
 using StoreApp.Web.Models;
@@ -7,14 +8,15 @@ namespace StoreApp.Web.Controllers;
 
 public class HomeController : Controller
 {
+    public int pageSize = 3;
     private IStoreRepository _storeRepository;
     public HomeController(IStoreRepository storeRepository)
     {
         _storeRepository = storeRepository;
     }
-    public IActionResult Index()
+    public IActionResult Index(int page = 1)
     {
-        var products = _storeRepository.Products.Select(p => new ProductViewModel
+        var products = _storeRepository.Products.Skip((page - 1) * pageSize).Select(p => new ProductViewModel
         {
             Id = p.Id,
             Name = p.Name,
@@ -24,7 +26,12 @@ public class HomeController : Controller
 
         return View(new ProductListViewModel
         {
-            Products = products
+            Products = products,
+            PageInfo = new PageInfo
+            {
+                ItemPerPage = pageSize,
+                TotalItems = _storeRepository.Products.Count()
+            }
         });
     }
 }
--- StoreApp.Web/Models/ProductViewModel.cs
+++ StoreApp.Web/Models/ProductViewModel.cs
@@ -15,4 +15,13 @@ public class ProductListViewModel
 {
     public IEnumerable<ProductViewModel> Products { get; set; } = Enumerable.Empty<ProductViewModel>();
 
+    public PageInfo PageInfo { get; set; } = new();
+
+}
+
+public class PageInfo
+{
+    public int TotalItems { get; set; }
+    public int ItemPerPage { get; set; }
+    public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemPerPage);
 }
